f190fbf2e8caeed17784c36fdd9337a2f69b4caa
luknuk <44519853+luknuk@users.noreply.github.com>
2021-04-30 09:42:42 +1000

    Fix CURL bug in which it throws an error when it receives a destination path

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 mkdir -p ~/bin
-curl -LJO https://raw.githubusercontent.com/luknuk/jrnl-sync/installscript/jrnl-sync.sh ~/bin/
-# git clone https://github.com/luknuk/jrnl-sync.git ~/bin
-mv ~/bin/jrnl-sync.sh ~/bin/jrnl-sync
+curl -LJO https://raw.githubusercontent.com/luknuk/jrnl-sync/installscript/jrnl-sync.sh
+mv jrnl-sync.sh ~/bin/jrnl-sync
 chmod +x ~/bin/jrnl-sync
 
 echo "Thank you for installing jrnl-sync. Please add the following to your .bashrc or .zshrc:
