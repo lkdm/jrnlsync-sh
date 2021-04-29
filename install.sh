52e9fd0ffb1af0e57f34334e01576b3989be4a61
luknuk <44519853+luknuk@users.noreply.github.com>
2021-04-30 09:37:45 +1000

    Bugfix: Remove .sh extension rather than add it after install.

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 mkdir -p ~/bin
-curl -LJO https://raw.githubusercontent.com/luknuk/jrnl-sync/installscript/jrnl-sync.sh ~/bin/jrnl-sync
+curl -LJO https://raw.githubusercontent.com/luknuk/jrnl-sync/installscript/jrnl-sync.sh ~/bin/
 # git clone https://github.com/luknuk/jrnl-sync.git ~/bin
-mv ~/bin/jrnl-sync ~/bin/jrnl-sync.sh
-chmod +x ~/bin/jrnl-sync/jrnl-sync.sh
+mv ~/bin/jrnl-sync.sh ~/bin/jrnl-sync
+chmod +x ~/bin/jrnl-sync
 
 echo "Thank you for installing jrnl-sync. Please add the following to your .bashrc or .zshrc:
 
